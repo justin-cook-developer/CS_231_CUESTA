9964878d0dac1a2d44c1fa39fc552ba96be716e8
Justin Cook <justin.cook.dev@gmail.com>
2019-08-30 20:33:27 -0700

    Finished lab 5.

---
+++ lab_5/script.py
@@ -0,0 +1,48 @@
+
+
+def isOdd(integer):
+    isEven = integer % 2 == 0
+    msg = None
+
+    if (isEven):
+        msg = str(integer) + ' is even!'
+    else:
+        msg = str(integer) + ' is odd!'
+
+    print(msg)
+
+
+def minutesToClosestHour(minutes):
+    pastHalfHour = minutes > 30
+
+    if (pastHalfHour):
+        minutesToLog = 60 - minutes
+        print(minutesToLog, 'before the hour')
+    else:
+        print(minutes, 'minutes after the hour')
+
+
+def comapreNums(x, y):
+    output = 'x: ' + str(x) + ', y: ' + str(y)
+
+    if (x > y):
+        output += ', x > y'
+    elif (x < y):
+        output += ', x < y'
+    else:
+        output += ', x == y'
+
+    print(output)
+
+
+def main():
+    x = int(input('Input an x value\n'))
+
+    isOdd(x)
+    minutesToClosestHour(x)
+
+    y = int(input('Input a y value\n'))
+    comapreNums(x, y)
+
+
+main()
