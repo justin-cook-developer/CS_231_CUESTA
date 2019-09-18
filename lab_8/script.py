6e18b97f57951c536e4a6821faba965018ba2c11
Justin Cook <justin.cook.dev@gmail.com>
2019-09-17 18:47:14 -0700

    Completed prinSlices.

--- lab_8/script.py
+++ lab_8/script.py
@@ -23,14 +23,45 @@ def filterOutChar(char, word):
     return new_word
 
 
-print("charCount('h', 'hello') == 1 :", charCount('h', 'hello') == 1)
-print("charCount('l', 'hello') == 2 :", charCount('l', 'hello') == 2)
-print("charCount('b', 'brother') == 1 :", charCount('b', 'brother') == 1)
+def printSlices(string):
+    length = len(string)
+
+    for i in range(length):
+        for j in range(i + 1, length + 1):
+            print(i, j, string[i:j])
 
 
-print("\nfilterOutChar('h', 'hello') == 'ello' :",
-      filterOutChar('h', 'hello') == 'ello')
-print("filterOutChar('l', 'hello') == 'heo' :",
-      filterOutChar('l', 'hello') == 'heo')
-print("filterOutChar('o', 'hello') == 'hell' :",
-      filterOutChar('o', 'hello') == 'hell')
+print(
+    "charCount('h', 'hello') == 1 :",
+    charCount('h', 'hello') == 1
+)
+print(
+    "charCount('l', 'hello') == 2 :",
+    charCount('l', 'hello') == 2
+)
+print(
+    "charCount('b', 'brother') == 1 :",
+    charCount('b', 'brother') == 1
+)
+
+
+print(
+    "\nfilterOutChar('h', 'hello') == 'ello' :",
+    filterOutChar('h', 'hello') == 'ello'
+)
+print(
+    "filterOutChar('l', 'hello') == 'heo' :",
+    filterOutChar('l', 'hello') == 'heo'
+)
+print(
+    "filterOutChar('o', 'hello') == 'hell' :",
+    filterOutChar('o', 'hello') == 'hell'
+)
+
+
+print('\n')
+printSlices('abc')
+print('\n')
+printSlices('hello')
+print('\n')
+printSlices('hi')
